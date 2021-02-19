e6e320cfc98fc811c68b397a8b8d9bf5ad39ee4b
Winston McGriff <winstonmcgriff27@gmail.com>
2021-02-19 03:28:37 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,21 +1,21 @@
 def join_nested_strings(mixed_data)
 
-  message = ""
-  row_index = 0 
-  
-  while row_index < mixed_data.count do 
-    
-    element_index = 0 
-     
-  while element_index < mixed_data[row_index].count do 
-    
-   #puts "bob" 
-    
-   if mixed_data[row_index][element_index].class == String
-   #puts "bob" 
-  message << mixed_data[row_index][element_index] + " "
+message = ""
+row_index = 0 
+
+while row_index < mixed_data.count do 
+
+element_index = 0 
+
+while element_index < mixed_data[row_index].count do 
+
+#puts "bob" 
+
+if mixed_data[row_index][element_index].class == String
+#puts "bob" 
+message << mixed_data[row_index][element_index] + " "
  binding.pry
-    
+
 end
 element_index += 1
 end 
